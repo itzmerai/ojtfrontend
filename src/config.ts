a7c36d713aa1daa874050786a22ff98bd6adf01c
itzmerai <159508958+itzmerai@users.noreply.github.com>
2025-03-29 13:27:56 +0800

    dotenve

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
     //API_BASE_URL: "https://newstructured-production.up.railway.app",
-    API_BASE_URL: "https://newstructured.vercel.app",
+    API_BASE_URL: import.meta.env.VITE_API_BASE_URL,    
     //API_BASE_URL: "http://localhost:5000",
   };
   
